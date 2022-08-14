18757151d9c40b91d007e5f60f8950eace15f59c
AliJH <aliharrison@outlook.com>
2022-08-14 13:58:59 +1000

    create unique list of given names, swap 'staging' to 'transformed'

--- get_data/get_names.py
+++ get_data/get_names.py
@@ -11,19 +11,24 @@
 
 
 ### Setup ###
-from bs4 import BeautifulSoup
+from bs4            import BeautifulSoup
 from urllib.request import urlopen
+from os             import listdir
+from os.path        import isfile, join
+import pandas       as p
 
-dir_project_root     = 'C:\Projects\Python\DatasetGenerator'
-dir_dataset_raw      = dir_project_root + '\datasets\\raw'
-dir_dataset_staging  = dir_project_root + '\datasets\staging'
-dir_dataset_modelled = dir_project_root + '\datasets\modelled'
+dir_project_root         = 'C:\Projects\Python\DatasetGenerator'
+dir_dataset_raw          = dir_project_root + '\datasets\\raw'
+dir_dataset_transformed  = dir_project_root + '\datasets\transformed'
+dir_dataset_modelled     = dir_project_root + '\datasets\modelled'
+
+dir_dataset_raw_given_names = dir_dataset_raw + '\given_names'
 
 
 ### Save Raw Given Names Data To Raw ###
 # Load HTML of the Top 100 Baby Names dataset. This page links to each of the
 # by year dataset pages. Each of these specific by year dataset pages has a
-# link to a csv version of the dataset which will download into our staging
+# link to a csv version of the dataset which will download into our transformed
 # directory.
 url_root        = 'https://data.qld.gov.au'
 url_names_root  = url_root + '/dataset/top-100-baby-names'
@@ -40,12 +45,29 @@ for url_current_dataset in relative_urls_names_datasets:
   fn_names_current_csv    = str.rsplit(url_names_current_csv, '/')[-1]
   dataset_current_csv     = urlopen(url_names_current_csv).read()
 
-  with open(dir_dataset_raw + '\given_names\\' + fn_names_current_csv, 'wb') as f:
+  with open(join(dir_dataset_raw_given_names, fn_names_current_csv), mode='wb') as f:
     f.write(dataset_current_csv)
 
 
-### Create Staging Dataset of Given Names With Sex ###
+### Create transformed Dataset of Given Names With Sex ###
+# Open all raw given names datasets and create a list of unique names with sex.
+# Using a dictionary with the key as the given name and value as the sex to
+# easily guarantee unique names. This does mean that where a given name appears
+# alongside multiple sexes the last appearance will determine the sex we use in
+# our dataset. For our purposes of getting a simple list of names relatively
+# quickly this is acceptable.
+unique_given_names = dict()
+def add_given_names_to_dict(dict, name, sex):
+  dict[name] = sex
 
+for f in listdir(dir_dataset_raw_given_names):
+  current_raw_file = join(dir_dataset_raw_given_names, f)
 
+  if isfile(current_raw_file):
+    current_dataset = p.read_csv(current_raw_file)
 
-### Create Modelled Dataset of Popular Names ###
+    if 'Name' in current_dataset.columns:
+      current_dataset.apply(lambda row: add_given_names_to_dict(unique_given_names, row['Name'], row['Sex']), axis='columns')
+    else:
+      current_dataset.apply(lambda row: add_given_names_to_dict(unique_given_names, row['Girl Names'], 'Female'), axis='columns')
+      current_dataset.apply(lambda row: add_given_names_to_dict(unique_given_names, row['Boy Names'], 'Male'), axis='columns')
